124beede839f18247e69c8d5440a9f871111e035
Kim Hyo Rim <59546818+KxxHyoRim@users.noreply.github.com>
2022-05-19 17:58:47 +0900

    Create bj17298.java

---
+++ baekjoon/bj17298.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()), curr;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Stack<Integer> s = new Stack<>();
+        i
